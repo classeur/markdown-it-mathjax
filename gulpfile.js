23bc057a9e8405ca48c46fe8e34ef9706201c4e3
benweet <benoit.schweblin@gmail.com>
2016-03-13 17:41:27 +0000

    Removed unnecessary token level

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,22 @@ var exec = clgulp.exec
 var util = clgulp.util
 var mocha = require('gulp-spawn-mocha')
 
-gulp.task('tag', ['test', 'lint'], function(cb) {
-	var version = require('./package').version
-	var tag = 'v' + version
-	util.log('Tagging as: ' + util.colors.cyan(tag))
-	exec([
-		'git add package.json',
-		'git commit -m "Prepare release"',
-		'git tag -a ' + tag + ' -m "Version ' + version + '"',
-		'git push origin master --tags',
-		'npm publish',
-	], cb)
+gulp.task('tag', ['test', 'lint'], function (cb) {
+  var version = require('./package').version
+  var tag = 'v' + version
+  util.log('Tagging as: ' + util.colors.cyan(tag))
+  exec([
+    'git add package.json',
+    'git commit -m "Prepare release"',
+    'git tag -a ' + tag + ' -m "Version ' + version + '"',
+    'git push origin master --tags',
+    'npm publish'
+  ], cb)
 })
 
-gulp.task('test', function() {
-	return gulp.src('test/*.js', {
-		read: false
-	})
-		.pipe(mocha())
+gulp.task('test', function () {
+  return gulp.src('test/*.js', {
+    read: false
+  })
+    .pipe(mocha())
 })
